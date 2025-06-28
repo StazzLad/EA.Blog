1a95a484dca333fcafd057cb0f96a059aa64e462
Erk Apaydin <erk.apaydin@protonmail.com>
2025-06-28 20:46:12 +0300

    Add project files.

---
+++ EA.WebAppUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EA.WebAppUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
